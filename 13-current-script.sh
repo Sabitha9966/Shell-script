3b560f821c028fa01ef1455300ee3ddea34ef74a
Sabitha9966 <sabithareddy36@gmail.com>
2024-04-28 15:05:45 +0530

    current-script

--- 13-current-script.sh
+++ 13-current-script.sh
@@ -5,12 +5,13 @@ COURSE="DevOps from Current Script"
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
+source ./14-other-script.sh
+
+
+echo "After calling other script, course: $COURSE"
+
 #./14-other-script.sh
 
 # with above syntax variable value will be same after calling other script.
 # with below syntax variable value willbe changed with the other scrip.
 
-source ./14-other-script.sh
-
-
-echo "After calling other script, course: $COURSE"
